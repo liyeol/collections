69d269d28485781d361591177a070b8b49eb139c
liyeol <98913681+liyeol@users.noreply.github.com>
2023-02-05 15:18:10 +0000

    v2

--- Program.cs
+++ Program.cs
@@ -1,61 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace VoteProgram
+namespace VotingApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> voteTopics = new Dictionary<string, int>();
+            // Dictionary to store voting topics and their respective options
+            Dictionary<string, List<string>> votingTopics = new Dictionary<string, List<string>>();
 
-            while (true)
+            // Dictionary to store user information and their respective votes
+            Dictionary<string, string> userVotes = new Dictionary<string, string>();
+
+            // Flag to indicate if the program should continue running
+            bool keepRunning = true;
+
+            while (keepRunning)
             {
-                Console.WriteLine("1. Create a vote topic");
+                // Display main menu
+                Console.WriteLine("1. Create a voting topic");
                 Console.WriteLine("2. Vote");
-                Console.WriteLine("3. Show results");
+                Console.WriteLine("3. View voting results");
                 Console.WriteLine("4. Exit");
-                Console.Write("Enter your choice: ");
 
+                Console.Write("Enter your choice: ");
                 int choice = int.Parse(Console.ReadLine());
 
                 switch (choice)
                 {
                     case 1:
-                        Console.Write("Enter the vote topic: ");
+                        Console.Write("Enter voting topic: ");
                         string topic = Console.ReadLine();
-                        Console.Write("Enter the options, separated by comma: ");
-                        string[] options = Console.ReadLine().Split(',');
 
-                        foreach (var option in options)
+                        Console.Write("Enter number of options: ");
+                        int numOptions = int.Parse(Console.ReadLine());
+
+                        List<string> options = new List<string>();
+                        for (int i = 0; i < numOptions; i++)
                         {
-                            voteTopics[option.Trim()] = 0;
+                            Console.Write($"Enter option {i + 1}: ");
+                            options.Add(Console.ReadLine());
                         }
+
+                        votingTopics[topic] = options;
                         break;
 
                     case 2:
-                        Console.Write("Enter the topic to vote for: ");
-                        string voteTopic = Console.ReadLine();
+                        Console.Write("Enter your first name: ");
+                        string firstName = Console.ReadLine();
 
-                        if (voteTopics.ContainsKey(voteTopic))
-                        {
-                            voteTopics[voteTopic]++;
-                            Console.WriteLine("Your vote has been recorded");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid topic");
-                        }
+                        Console.Write("Enter your last name: ");
+                        string lastName = Console.ReadLine();
+
+                        Console.Write("Enter your date of birth (DD.MM.YYYY): ");
+                        string dateOfBirth = Console.ReadLine();
+
+                        Console.Write("Enter voting topic: ");
+                        topic = Console.ReadLine();
+
+                        Console.Write("Enter your vote: ");
+                        string vote = Console.ReadLine();
+
+                        userVotes[firstName + " " + lastName + " " + dateOfBirth] = vote;
                         break;
 
                     case 3:
-                        Console.WriteLine("Results:");
-                        foreach (var top in voteTopics)
+
+                        Console.WriteLine("1. View results by user");
+                        Console.WriteLine("2. View results by voting topic");
+
+                        Console.Write("Enter your choice: ");
+                        int viewChoice = int.Parse(Console.ReadLine());
+
+                        switch (viewChoice)
                         {
-                            Console.WriteLine(top.Key + ": " + top.Value);
+                            case 1:
+
+                                foreach (KeyValuePair<string, string> userVote in userVotes)
+                                {
+                                    Console.WriteLine($"{userVote.Key}: {userVote.Value}");
+                                }
+                                break;
+
+                            case 2:
+
+                                foreach (KeyValuePair<string, List<string>> votingTopic in votingTopics)
+                                {
+                                    Console.WriteLine($"Results for {votingTopic.Key}:");
+                                }
+                                break;
+
                         }
                         break;
-
                     case 4:
                         return;
                 }
